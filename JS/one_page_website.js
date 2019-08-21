cb8c0d8a2f8304bdf8242b44c97dc98ce9c2cfdc
Tyler Johnson <tylerjohnson139@gmail.com>
2019-08-20 21:52:08 -0700

    fix class names and css

--- JS/one_page_website.js
+++ JS/one_page_website.js
@@ -17,24 +17,20 @@ function plusSlides(n) {
 }
 
 //Thumnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function currentSlide(i) {
+    showSlides(slideIndex = i);
 }
 
-function showSlides(n) {
-    var i;
+function showSlides(a) {
+    var a1;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i=0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i=0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    if (a > slides.length) {slideIndex = 1}
+    if (a < 1) {slideIndex = slides.length}
+    for (b=0; b < slides.length; b++) {
+        slides[b].style.display = "none";
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += "active";
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
